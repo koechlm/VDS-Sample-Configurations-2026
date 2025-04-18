05acaf5e88aeac2d9ccadf4a3f6bb16c95c35a69
Markus Koechl <markus.koechl@autodesk.com>
2025-04-18 18:24:23 +0200

    added navigation to CAD BOM

--- VDS-PDMC-Sample/Vault.Custom/addinVault/ADSK.QS.FileBOM.ps1
+++ VDS-PDMC-Sample/Vault.Custom/addinVault/ADSK.QS.FileBOM.ps1
@@ -84,12 +84,82 @@ function GetFileBOM($fileID)
 			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Material"}
 			$prop = $cldCompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id} | Select-Object -First 1
 			$bomItem.Material = $prop.Val
+			#add the name for navigation
+			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Name"}
+			$prop = $cldCompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id} | Select-Object -First 1
+			$bomItem.Name = $prop.Val
 		}
 	}
 	return $bomItems	
 }
 
-function mGoToCadBomComp{
+function mGoToCadBomCompFile
+{
 	$selectedBomRow = $dsWindow.FindName("bomList").SelectedItem
-	$dsDiag.Inspect("selectedBomRow")
+	[System.Reflection.Assembly]::LoadFrom('C:\Program Files\Autodesk\Autodesk Vault 2026 SDK\bin\x64\Autodesk.Connectivity.Explorer.ExtensibilityTools.dll')
+	[Autodesk.Connectivity.Explorer.ExtensibilityTools.IExplorerUtil]$mExplorerUtil = [Autodesk.Connectivity.Explorer.ExtensibilityTools.ExplorerLoader]::GetExplorerUtil($VaultApplication)
+		
+	#search a file to navigate to. Note the search returns the first file found; ensure that part number is unique in the vault
+	$mFile = mSearchFileByPartNumber($selectedBomRow.PartNumber)
+	
+	# convert the file to an IEntity file object
+	[Autodesk.DataManagement.Client.Framework.Vault.Currency.Entities.FileIteration]$mFileIteration = New-Object Autodesk.DataManagement.Client.Framework.Vault.Currency.Entities.FileIteration($vaultconnection, $mFile)
+
+	#navigate to the file in the vault explorer
+	$mExplorerUtil.GoToEntity($mFileIteration)
+}
+
+function mGoToCadBomCompItem
+{
+	$selectedBomRow = $dsWindow.FindName("bomList").SelectedItem
+	[System.Reflection.Assembly]::LoadFrom('C:\Program Files\Autodesk\Autodesk Vault 2026 SDK\bin\x64\Autodesk.Connectivity.Explorer.ExtensibilityTools.dll')
+	[Autodesk.Connectivity.Explorer.ExtensibilityTools.IExplorerUtil]$mExplorerUtil = [Autodesk.Connectivity.Explorer.ExtensibilityTools.ExplorerLoader]::GetExplorerUtil($VaultApplication)
+		
+	#search a file to navigate to. Note the search returns the first file found; ensure that part number is unique in the vault
+	$mFile = mSearchFileByPartNumber($selectedBomRow.PartNumber)
+	
+	#get the first linked item from the file
+	$mItem = $vault.ItemService.GetItemsByFileId($mFile.Id)[0]
+
+	if ($null -ne $mItem) {
+		# convert the item to an IEntity file object
+		[Autodesk.DataManagement.Client.Framework.Vault.Currency.Entities.ItemRevision]$mItemRevision = New-Object Autodesk.DataManagement.Client.Framework.Vault.Currency.Entities.ItemRevision($vaultconnection, $mItem)
+		
+		#navigate to the item in the vault explorer
+		$mExplorerUtil.GoToEntity($mItemRevision)
+	}
+}
+
+function mSearchFileByPartNumber([String]$PartNumber)
+{
+	$mSearchString = $selectedBomRow.PartNumber
+	$srchCond = New-Object autodesk.Connectivity.WebServices.SrchCond
+	$propDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId("FILE")
+	$propDef = $propDefs | Where-Object { $_.SysName -eq "PartNumber" }
+	$srchCond.PropDefId = $propDef.Id
+	$srchCond.SrchOper = 3
+	$srchCond.SrchTxt = $mSearchString
+	$srchCond.PropTyp = [Autodesk.Connectivity.WebServices.PropertySearchType]::SingleProperty
+	$srchCond.SrchRule = [Autodesk.Connectivity.WebServices.SearchRuleType]::Must
+
+	$mSearchStatus = New-Object autodesk.Connectivity.WebServices.SrchStatus
+	$srchSort = New-Object Autodesk.Connectivity.WebServices.SrchSort
+	#$srchSort
+	$mBookmark = ""     
+	$mResultAll = New-Object 'System.Collections.Generic.List[Autodesk.Connectivity.WebServices.File]'
+
+	while (($mSearchStatus.TotalHits -eq 0) -or ($mResultAll.Count -lt $mSearchStatus.TotalHits)) {
+		$mResultPage = $vault.DocumentService.FindFilesBySearchConditions(@($srchCond), @($srchSort), @(($vault.DocumentService.GetFolderRoot()).Id), $true, $true, [ref]$mBookmark, [ref]$mSearchStatus)
+		#check the indexing status; you might return a warning that the result bases on an incomplete index, or even return with a stop/error message, that we need to have a complete index first
+		If ($mSearchStatus.IndxStatus -eq "IndexingComplete" -or $mSearchStatus -eq "IndexingContent") {
+		}
+		if ($mResultPage.Count -ne 0) {
+			$mResultAll.AddRange($mResultPage)
+		}
+		else { break; }
+			
+		break; #limit the search result to the first result page; page scrolling not implemented in this snippet release
+	}
+
+	return $mResultAll[0]
 }
