00b9abde4ce0b4220195166e6c3500c554e879e0
Markus Koechl <markus.koechl@autodesk.com>
2025-04-23 19:30:52 +0200

    initial draft reading model state BOMs

--- VDS-PDMC-Sample/Vault.Custom/addinVault/ADSK.QS.FileBOM.ps1
+++ VDS-PDMC-Sample/Vault.Custom/addinVault/ADSK.QS.FileBOM.ps1
@@ -1,5 +1,4 @@
-﻿class myBom
-{
+﻿class mBomRow {
 	[string] $Position
 	[string] $PartNumber
 	[string] $ComponentType
@@ -11,90 +10,155 @@
 	[string] $Material
 }
 
-function GetFileBOM($fileID)
-{
+class mBom {
+	[mBomRow[]] $BOMItems	
+}
+
+function mGetMdlStates($fileID) {
+		
+	$global:mFileBOM = $vault.DocumentService.GetBOMByFileId($fileID)
+	$dsDiag.Inspect("mFileBOM")
+	$MsArray = @()
+	$MsArray += $mFileBom.CompArray | Where-Object { $_.XRefId -eq -1 -and $_.UniqueId -like "MS:*" } # model state BOMs are internal component BOMs
+	
+	if ($MsArray.Count -gt 1) {
+		$mMdlStates = @{}
+		$MsArray | ForEach-Object { 
+			$mName = ""
+			if($_.Name -like "* [*]") {
+				#get the model state name
+				$mName = "[Primary]"
+			}
+			if ($_.Name -like "* (*)") {
+				#get the model state name
+				$mName = $_.Name.Split(" (")[1]
+				$mName = $mName.Substring(0, $mName.Length - 1)					
+			}	#add the primary state name
+			
+			
+			<# $msNames = $_.Name.Split(" (")
+			if ($msNames.Count -gt 1) {
+				#get the model state name
+				$mName = $msNames.LastOrDefault()
+			}
+			else {
+				#add the primary state name
+				$mName = "[" + $msNames[0].Split("[")[1]
+			} #>
+
+			$mMdlStates.Add($mName, $_.Id)
+
+			
+		}			
+		$dsWindow.FindName("cmbModelStates").ItemsSource = $mMdlStates
+		$dsWindow.FindName("cmbModelStates").SelectedIndex = 0
+		$dsWindow.FindName("cmbModelStates").IsEnabled = $true
+	}
+	else {
+		$dsWindow.FindName("cmbModelStates").IsEnabled = $false
+		$dsWindow.FindName("cmbModelStates").SelectedIndex = -1
+		$dsWindow.FindName("cmbModelStates").ItemsSource = $null
+	}
+
+	return $MsArray
+}
+
+function GetFileBOM($fileID, $BomCompId) {
 	#Get Thumbnail PropertyDefinition 
 	$propDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId('FILE')
-	$thumbnailPropDef = $propDefs | Where-Object {$_.SysName -eq 'Thumbnail'}
+	$thumbnailPropDef = $propDefs | Where-Object { $_.SysName -eq 'Thumbnail' }
 
-	$bom = $vault.DocumentService.GetBOMByFileId($fileID)
-	$cldIds =@()
-	$bom.InstArray | Where-Object { $_.ParId -eq 0 } | ForEach-Object { 
+	if ($null -eq $Global:mFileBOM) {
+		$global:mFileBom = $vault.DocumentService.GetBOMByFileId($fileID)
+	}	
+
+	$cldIds = @()
+	$mFileBom.InstArray | Where-Object { $_.ParId -eq $BomCompId } | ForEach-Object { 
+		[autodesk.Connectivity.WebServices.BOMInst]$mBomInst = $_
+		#$dsDiag.Inspect("mBomInst")
 		$CldId = $_.CldId
-		$comp = $bom.CompArray | Where-Object { $_.Id -eq $CldId }
+		$comp = $mFileBom.CompArray | Where-Object { $_.Id -eq $CldId }
+		#$dsDiag.Inspect("comp")		
 		if ($comp.XRefId -ne -1) {
 			$cldIds += $comp.XRefId
 		}
 	}
+	#$dsDiag.Inspect("cldIds")
 
 	$bomItems = @()
-	if($cldIds.Count -gt 0) #the file contains BOM information, so continue
-	{
+	if ($cldIds.Count -gt 0) {
+		#the file contains BOM information, so continue
 		$CldBoms = $vault.DocumentService.GetBOMByFileIds($cldIds)
-		$schm = $bom.SchmArray | Where-Object { $_.SchmTyp -eq "Structured" -and $_.RootCompId -eq 0}
+		$schm = $mFileBom.SchmArray | Where-Object { $_.SchmTyp -eq "Structured" -and $_.RootCompId -eq $BomCompId }
 		$cldBomCounter = 0
-
-		$bom.InstArray | Where-Object { $_.ParId -eq 0 } | ForEach-Object {
-			$bomItem = New-Object myBom
-			$CldId = $_.CldId
-			if ($_.QuantOverde -eq -1)
-			{
+		$dsDiag.Inspect("schm")
+		$mFileBom.InstArray | Where-Object { $_.ParId -eq $BomCompId } | ForEach-Object {
+			[autodesk.Connectivity.WebServices.BOMInst]$mBomInst = $_
+			$dsDiag.Inspect("mBomInst")
+			$bomItem = New-Object mBomRow
+			$CldId = $_.CldId #$BomCompId
+			if ($_.QuantOverde -eq -1) {
 				$bomItem.Quantity = $_.Quant
 			}
 			else {
 				$bomItem.Quantity = $_.QuantOverde
 			}
-			$comp = $bom.CompArray | Where-Object { $_.Id -eq $CldId }
-
-			$occur = $bom.SchmOccArray | Where-Object { $_.SchmId -eq $schm.Id -and $_.CompId -eq $CldId }
+			$comp = $mFileBom.CompArray | Where-Object { $_.Id -eq $CldId }
+			$dsDiag.Inspect("comp")
+			$occur = $mFileBom.SchmOccArray | Where-Object { $_.SchmId -eq $schm.Id -and $_.CompId -eq $CldId }
 			$bomItem.Position = $occur.DtlId
-
+			$dsDiag.Inspect("occur")
 			if ($comp.XRefId -eq -1) {
-				$cldBom = $bom
+				$cldBom = $mFileBom
 			}
 			else {
 				$cldBom = $CldBoms[$cldBomCounter++]
 			}
 			$UniqueId = $comp.UniqueId
 			#find component in current file bom only
-			$cldComp = $cldBom.CompArray | Where-Object { $_.UniqueId -eq $UniqueId -and $_.XRefId -eq -1} | Select-Object -First 1
-			if(-not $cldComp){
+			$cldComp = $cldBom.CompArray | Where-Object { $_.UniqueId -eq $UniqueId -and $_.XRefId -eq -1 } #| Select-Object -First 1
+			$dsDiag.Inspect("cldComp")
+			if (-not $cldComp) {
 				$cldComp = $cldBom.CompArray[0]
 			}
 			$bomItem.Name = $cldComp.Name
 			$bomItem.ComponentType = $cldComp.CompTyp
-			$cldCompAttrArray = $cldBom.CompAttrArray | Where-Object { $_.CompId -eq $cldComp.Id}
-			if($cldCompAttrArray.Count -eq 0){
+			$cldCompAttrArray = $cldBom.CompAttrArray | Where-Object { $_.CompId -eq $cldComp.Id }
+			if ($cldCompAttrArray.Count -eq 0) {
 				$cldCompAttrArray = $cldBom.CompAttrArray
 			}
-			$PropPartNumber = $cldBom.PropArray | Where-Object { $_.dispName -eq "Part Number"}
-			$prop = ($cldCompAttrArray | Where-Object { $_.PropId -eq $PropPartNumber.Id}) | Select-Object -First 1
+			$PropPartNumber = $cldBom.PropArray | Where-Object { $_.dispName -eq "Part Number" }
+			$prop = ($cldCompAttrArray | Where-Object { $_.PropId -eq $PropPartNumber.Id }) | Select-Object -First 1
 			$bomItem.PartNumber = $prop.Val
 			$bomItems += $bomItem
-
+			$dsDiag.Inspect("prop")
 			#add Inventor default BOM columns
 			$thumbnailProp = $vault.PropertyService.GetProperties('FILE', @($cldIds[$cldBomCounter - 1]), @($thumbnailPropDef.Id))[0]
 			$bomItem.Thumbnail = $thumbnailProp.Val
-			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Title"}
-			$prop = $cldCompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id} | Select-Object -First 1
+			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Title" }
+			$prop = $cldCompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id } | Select-Object -First 1
 			$bomItem.Title = $prop.Val
-			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Description"}
-			$prop = $cldCompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id} | Select-Object -First 1
+			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Description" }
+			$prop = $cldCompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id } | Select-Object -First 1
 			$bomItem.Description = $prop.Val
-			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Material"}
-			$prop = $cldCompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id} | Select-Object -First 1
-			$bomItem.Material = $prop.Val
-			#add the name for navigation
-			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Name"}
-			$prop = $cldCompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id} | Select-Object -First 1
-			$bomItem.Name = $prop.Val
-		}
+			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Material" }
+			$prop = $cldCompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id } | Select-Object -First 1
+			$bomItem.Material = $prop.Val					
+		}				
 	}
-	return $bomItems	
-}
+	$global:mPrimaryBOM = New-Object mBom
+	$global:mPrimaryBOM.BOMItems += $bomItems
 
-function mGoToCadBomCompFile
-{
+	$dsDiag.Inspect("bomItems")
+	return $bomItems
+
+	#$mBoms += $compBOM
+	#$dsDiag.Inspect()
+	#return $mBom[0].BOMItems[0]
+}
+	
+
+function mGoToCadBomCompFile {
 	$selectedBomRow = $dsWindow.FindName("bomList").SelectedItem
 	[System.Reflection.Assembly]::LoadFrom('C:\Program Files\Autodesk\Autodesk Vault 2026 SDK\bin\x64\Autodesk.Connectivity.Explorer.ExtensibilityTools.dll')
 	[Autodesk.Connectivity.Explorer.ExtensibilityTools.IExplorerUtil]$mExplorerUtil = [Autodesk.Connectivity.Explorer.ExtensibilityTools.ExplorerLoader]::GetExplorerUtil($VaultApplication)
@@ -109,8 +173,7 @@ function mGoToCadBomCompFile
 	$mExplorerUtil.GoToEntity($mFileIteration)
 }
 
-function mGoToCadBomCompItem
-{
+function mGoToCadBomCompItem {
 	$selectedBomRow = $dsWindow.FindName("bomList").SelectedItem
 	[System.Reflection.Assembly]::LoadFrom('C:\Program Files\Autodesk\Autodesk Vault 2026 SDK\bin\x64\Autodesk.Connectivity.Explorer.ExtensibilityTools.dll')
 	[Autodesk.Connectivity.Explorer.ExtensibilityTools.IExplorerUtil]$mExplorerUtil = [Autodesk.Connectivity.Explorer.ExtensibilityTools.ExplorerLoader]::GetExplorerUtil($VaultApplication)
@@ -130,8 +193,7 @@ function mGoToCadBomCompItem
 	}
 }
 
-function mSearchFileByPartNumber([String]$PartNumber)
-{
+function mSearchFileByPartNumber([String]$PartNumber) {
 	$srchCond = New-Object autodesk.Connectivity.WebServices.SrchCond
 	$propDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId("FILE")
 	$propDef = $propDefs | Where-Object { $_.SysName -eq "PartNumber" }
